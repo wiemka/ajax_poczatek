151ccc540a1eb50442b0727a1586cdeeb12fed3b
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-27 10:04:07 +0200

    dodanie drugiego sposobu

--- js/ajax_jq.js
+++ js/ajax_jq.js
@@ -6,9 +6,20 @@ $(function () {
         dataType: "json",
         success: function (resultJSON) {
             console.log(resultJSON);
+
+            var pUserId = document.createElement("p");
+            var pUserName = document.createElement("p");
+            var pUserUrl = document.createElement("p");
+
+            pUserId = "User ID:" +
         },
         onerror: function (msg) {
             console.log(msg);
         }
     });
+
+    //drugi sposób
+    //    $.getJSON("http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl", function (data) {
+    //        console.log(data);
+    //    });
 });
